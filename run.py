33267024eab4beda1cc897331cc439dec752c33a
이승헌 [heonbert] <heonbert@nexon-gt.com>
2016-03-22 19:04:57 +0900

    Democratic Added !

--- run.py
+++ run.py
@@ -174,6 +174,7 @@ if __name__ == "__main__":
   from nab.detectors.skyline.skyline_detector import SkylineDetector
   from nab.detectors.random.random_detector import RandomDetector
   from nab.detectors.null.null_detector import NullDetector
+  from nab.detectors.democratic.democratic_detector import DemocraticDetector
 
   if args.skipConfirmation or checkInputs(args):
     main(args)
